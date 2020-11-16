643f7bc4aad3a24dce07be830cfd3dba9064624a
Ankita Nigam <ankitanigam51@gmail.com>
2020-11-16 16:01:10 +0530

    answer

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,6 @@
+
+
+
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
@@ -11,10 +14,11 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.arrowKeyPressed = this.arrowKeyPressed.bind(this)
     };
 
     buttonClickHandler() {
-   
+        this.setState({renderBall: true}) 
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
@@ -24,14 +28,26 @@ class App extends Component {
 		}
     }
 
+    arrowKeyPressed(evt) {
+      if(evt.keyCode === 39){
+        if(this.state.renderBall){
+            const newPosI = this.state.posi + 5;
+            this.setState ({
+                posi: newPosI,
+                ballPosition: { left: newPosI + "px" }
+            })
+        }
+      }
+    }
+
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+        document.addEventListener("keydown",this.arrowKeyPressed);
     }
 
     render() {
         return (
-            <div className="playground">
+            <div className="playground" onKeyPress={this.arrowKeyPressed}>
                 {this.renderBallOrButton()}
             </div>
         )
